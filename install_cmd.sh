e38f20cc6f2f7ebf63f075d927ddb4b8ed61970d
MATOO-Dev <matoo-dev@proton.me>
2026-03-07 14:31:28 +0100

    Reworked install cmd script again

--- install_cmd.sh
+++ install_cmd.sh
@@ -1,17 +1,19 @@
-# abbreviations
-disko=github:nix-community/disko/latest#disko-install
-experimental=--extra-experimental-features
-extras="\"nix-command flakes\""
-options=--write-efi-boot-entries
-
 # fixes 'no space left on device' during install
 export TMPDIR=/mnt/flake/tmp
 
 # install desktop
-# sudo nix run $disko $experimental -- $options --flake .#matoo-desktop --disk primary /dev/nvme0n1 secondary /dev/nvme1n1 tertiary /dev/sda
+sudo nix run \
+	github:nix-community/disko/latest#disko-install \
+	--extra-experimental-features "nix-command flakes" \
+	-- --write-efi-boot-entries --flake \
+	# desktop
+	# .#matoo-desktop \
+	# --disk primary /dev/nvme0n1 secondary /dev/nvme1n1 tertiary /dev/sda
 
-# install laptop
-# sudo nix run $disko $experimental -- $options --flake .#matoo-laptop --disk primary /dev/nvme0n1
+	# laptop
+	# .#matoo-laptop \
+	# --disk primary /dev/nvme0n1
 
-# install vm
-# sudo nix run $disko $experimental -- $options --flake .#matoo-vm --disk primary /dev/vda
+	# vm
+	# .#matoo-vm \
+	# --disk primary /dev/vda

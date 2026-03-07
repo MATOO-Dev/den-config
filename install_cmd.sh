b2414f07ba7fd1618dfa3330bd0066a2ae30099a
MATOO-Dev <matoo-dev@proton.me>
2026-03-07 14:21:51 +0100

    Reworked install cmd script

--- install_cmd.sh
+++ install_cmd.sh
@@ -1,6 +1,16 @@
-# disko=github:nix-community/disko/latest#disko-install
-# experimental=--extra-experimental-features "nix-command flakes"
-sudo nix run github:nix-community/disko/latest#disko-install --extra-experimental-features "nix-command flakes" -- --write-efi-boot-entries --flake flake.nix#<flake_attribute> --disk primary /dev/vda
-# sudo nix run $disko $experimental -- --write-efi-boot-entries --flake flake.nix#matoo-vm --disk primary /dev/vda
-# sudo nix run $disko $experimental -- --write-efi-boot-entries --flake flake.nix#matoo-laptop --disk primary /dev/nvme0n1
-# sudo nix run $disko $experimental -- --write-efi-boot-entries --flake flake.nix#matoo-desktop --disk primary /dev/nvme0n1 --disk secondary /dev/nvme1n1 --disk tertiary /dev/sda
+# abbreviations
+disko=github:nix-community/disko/latest#disko-install
+experimental=--extra-experimental-features "nix-command flakes"
+options=--write-efi-boot-entries
+
+# fixes 'no space left on device' during install
+export TMPDIR=/mnt/flake/tmp
+
+# install desktop
+# sudo nix run $disko $experimental -- $options --flake .#matoo-desktop --disk primary /dev/nvme0n1 secondary /dev/nvme1n1 tertiary /dev/sda
+
+# install laptop
+# sudo nix run $disko $experimental -- $options --flake .#matoo-laptop --disk primary /dev/nvme0n1
+
+# install vm
+# sudo nix run $disko $experimental -- $options --flake .#matoo-vm --disk primary /dev/vda

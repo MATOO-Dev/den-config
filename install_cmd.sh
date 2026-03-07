b270d0360648b3441e7085f6de6efc644b518c52
MATOO-Dev <matoo-dev@proton.me>
2026-03-07 14:49:50 +0100

    Reduced default aspects for vm-install

--- install_cmd.sh
+++ install_cmd.sh
@@ -1,11 +1,17 @@
 # fixes 'no space left on device' during install
-export TMPDIR=/mnt/flake/tmp
+# export TMPDIR=/mnt/flake/tmp
 
 # install desktop
 sudo nix run \
 	github:nix-community/disko/latest#disko-install \
 	--extra-experimental-features "nix-command flakes" \
 	-- --write-efi-boot-entries --flake \
+
+	# editing note:
+	# uncomment the 2 lines below the configuration
+	# move them up to be directly below the rest of the command
+	# do not leave an empty line!
+
 	# desktop
 	# .#matoo-desktop \
 	# --disk primary /dev/nvme0n1 secondary /dev/nvme1n1 tertiary /dev/sda

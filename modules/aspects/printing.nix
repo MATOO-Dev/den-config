f317d47c143e5a061fa93f5d0781e3f3edbba56f
MATOO-Dev <matoo-dev@proton.me>
2026-03-09 11:54:50 +0100

    Fixed printing aspect syntax

--- modules/aspects/printing.nix
+++ modules/aspects/printing.nix
@@ -1,30 +1,32 @@
 {
-	den.aspects.printing.nixos = { pkgs, user, ... }: {
-		# enable CUPS to print documents
-		services.printing = {
-			enable = true;
-			drivers = with pkgs; [
-				hplip
+	den.aspects.printing = {pkgs, user, ...}: {
+		nixos = { ... }: {
+			# enable CUPS to print documents
+			services.printing = {
+				enable = true;
+				drivers = with pkgs; [
+					hplip
+				];
+			};
+
+			# is this line needed if i already defined it above?
+			# ill leave it commented for now and test it next time i need to print
+			# HP printer driver
+			# environment.systemPackages = with pkgs; [ hplip ];
+
+			# enable scanning
+			hardware.sane = {
+				enable = true;
+				extraBackends = with pkgs; [
+					hplipWithPlugin
+					sane-airscan
+				];
+			};
+
+			users.users.${user.userName}.extraGroups = [
+				"lp" # printing
+				"scanner" # scanning
 			];
 		};
-
-		# is this line needed if i already defined it above?
-		# ill leave it commented for now and test it next time i need to print
-		# HP printer driver
-		# environment.systemPackages = with pkgs; [ hplip ];
-
-		# enable scanning
-		hardware.sane = {
-			enable = true;
-			extraBackends = with pkgs; [
-				hplipWithPlugin
-				sane-airscan
-			];
-		};
-
-		users.users.${user.userName}.extraGroups = [
-			"lp" # printing
-			"scanner" # scanning
-		];
 	};
 }

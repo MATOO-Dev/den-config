d6a745821cf62c2fde15e48cc293b3d0d29a00c6
MATOO-Dev <matoo-dev@proton.me>
2026-03-11 12:04:29 +0100

    Restored favicon links

--- modules/aspects/firefox.nix
+++ modules/aspects/firefox.nix
@@ -111,25 +111,25 @@
 						nix-packages = {
 							name = "Nix package search";
 							urls = [{ template = "https://search.nixos.org/packages?query={searchTerms}"; }];
-							# icon = "https://nixos.org/favicon.ico";
+							icon = "https://nixos.org/favicon.ico";
 							definesAliases = [ "@np" ];
 						};
 						nix-options = {
 							name = "Nix option search";
 							urls = [{ template = "https://search.nixos.org/options?query={searchTerms}"; }];
-							# icon = "https://nixos.org/favicon.ico";
+							icon = "https://nixos.org/favicon.ico";
 							definesAliases = [ "@no" ];
 						};
 						nixos-wiki = {
 							name = "NixOS wiki";
 							urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
-							# icon = "https://wiki.nixos.org/favicon.ico";
+							icon = "https://wiki.nixos.org/favicon.ico";
 							definesAliases = [ "@nw" ];
 						};
 						mynixos = {
 							name = "MyNixOS";
 							urls = [{ template = "https://mynixos.com/search?q={searchTerms}"; }];
-							# icon = "https://mynixos.com/favicon.ico";
+							icon = "https://mynixos.com/favicon.ico";
 							definesAliases = [ "@ns" ];
 						};
 					};

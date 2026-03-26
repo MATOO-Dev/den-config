b534b97b32fe3c68102489365927116a2d57f009
MATOO-Dev <matoo-dev@proton.me>
2026-03-26 03:02:48 +0100

    Added gitsigns setup opts

--- modules/nvim/vimGit.nix
+++ modules/nvim/vimGit.nix
@@ -2,25 +2,25 @@
 	den.aspects.vimGit.vim= {
 		git.gitsigns = {
 			enable = true;
-			# setupOpts = {
-			# 	signs = {
-			# 		add.text = "+┃";
-			# 		change.text = "~┃";
-			# 		delete.text = "-┃";
-			# 		topdelete.text = "-┃";
-			# 		changedelete.text = "~┃";
-			# 		untracked.text = "?┃";
-			# 	};
-			# 	signs_staged = {
-			# 		add.text = "+┃";
-			# 		change.text = "~┃";
-			# 		delete.text = "-┃";
-			# 		topdelete.text = "-┃";
-			# 		changedelete.text = "~┃";
-			# 		untracked.text = "?┃";
-			# 	};
-			# 	signs_staged_enable = true;
-			# };
+			setupOpts = {
+				signs = {
+					add.text = "+┃";
+					change.text = "~┃";
+					delete.text = "-┃";
+					topdelete.text = "-┃";
+					changedelete.text = "~┃";
+					untracked.text = "?┃";
+				};
+				signs_staged = {
+					add.text = "+┃";
+					change.text = "~┃";
+					delete.text = "-┃";
+					topdelete.text = "-┃";
+					changedelete.text = "~┃";
+					untracked.text = "?┃";
+				};
+				signs_staged_enable = true;
+			};
 		};
 
 		git.git-conflict = {

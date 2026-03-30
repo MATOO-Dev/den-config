08b32a4fd593d7ea6f70067934c238a54f0fbd02
MATOO-Dev <matoo-dev@proton.me>
2026-03-30 11:51:45 +0200

    Temporary workaround for blink keymap presets not working

--- modules/nvim/vimAutocomplete.nix
+++ modules/nvim/vimAutocomplete.nix
@@ -1,21 +1,31 @@
 {
-	den.aspects.vimAutocomplete.vim = {
-		autocomplete.blink-cmp = {
-			enable = true;
-			friendly-snippets.enable = true;
+	den.aspects.vimAutocomplete.vim = { pkgs, ... }: {
+		# autocomplete.blink-cmp = {
+		# 	enable = true;
+		# 	friendly-snippets.enable = true;
+		# 	setupOpts = {
+		# 		# appears to be bugged
+		# 		keymap.preset = "super-tab";
+		# 		# cmdline.keymap.preset = "super-tab";
+		# 		# completion.documentation.auto_show = true;
+		# 		# completion.list.selection.auto_insert = false;
+		# 		# signature.enabled = true;
+		# 		# sources.default = [
+		# 		# 	"lsp"
+		# 		# 	"path"
+		# 		# 	"snippets"
+		# 		# 	"buffer"
+		# 		# ];
+		# 	};
+		# };
+		
+		lazy.plugins."blink.cmp" = {
+			package = pkgs.vimPlugins.blink-cmp;
+			setupModule = "blink.cmp";
 			setupOpts = {
-				# appears to be bugged
-				cmdline.keymap.preset = "super-tab";
-				completion.documentation.auto_show = true;
-				completion.list.selection.auto_insert = false;
-				signature.enabled = true;
-				sources.default = [
-					"lsp"
-					"path"
-					"snippets"
-					"buffer"
-				];
+				keymap.preset = "super-tab";
 			};
+			event = [ "InsertEnter" ];
 		};
 	};
 }

df7ab50aa45077c46fa54ae4aa21f3eafb775b74
MATOO-Dev <matoo-dev@proton.me>
2026-03-31 00:57:18 +0200

    Updated virtualization aspect

--- modules/aspects/virtualization.nix
+++ modules/aspects/virtualization.nix
@@ -1,6 +1,12 @@
 {
 	den.aspects.virtualization.nixos = { ... }: {
-		virtualisation.libvirtd.enable = true;
 		programs.virt-manager.enable = true;
+		virtualisation.libvirtd = {
+			enable = true;
+			qemu = {
+				runAsRoot = true;
+				swtpm.enable = true;
+			};
+		};
 	};
 }

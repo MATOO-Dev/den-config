21cffa22b87332a6da66b341af5aad018532c93b
MATOO-Dev <matoo-dev@proton.me>
2026-03-31 15:59:57 +0200

    Added binary to find file mime type

--- modules/aspects/defaultapps.nix
+++ modules/aspects/defaultapps.nix
@@ -1,9 +1,16 @@
 {
-    den.aspects.defaultapps.home-manager = {
+    den.aspects.defaultapps.home-manager = { pkgs, ... }: {
+		home.packages = [
+			# requires file input to work
+			# test if syntax is correct like this
+			(pkgs.writeShellScriptBin "find-mime-type" ''
+				${pkgs.file}/bin/file --mime-type -b
+			'')
+		];
+
         xdg.mimeApps = {
             enable = true;
             # types can be found using 'file --mime-type -b <file>`
-            # todo: use writeShellScriptBin to make a simple binary for this
             defaultApplications = {
                 "applications/pdf" = "evince.desktop";
             };

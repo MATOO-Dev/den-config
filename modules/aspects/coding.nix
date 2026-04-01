8832a8d42ac898beda6a5e63612d327b5b0504d3
MATOO-Dev <matoo-dev@proton.me>
2026-04-01 14:43:49 +0200

    Removed qt creator from home config (moved to dev shell)

--- modules/aspects/coding.nix
+++ modules/aspects/coding.nix
@@ -1,22 +1,24 @@
 {
-	den.aspects.coding = {
-		homeManager = { pkgs, ... }: {
-			home.packages = with pkgs; [
-				assimp # asset importing library
-				bacon # rust code checker
-				cargo-deny # cargo denpendency linting / license checking
-				clang # clang compiler
-				clang-tools # additional tools for clang
-				gdb # c++ / rust compiler
-				# glad # opengl function pointer loader (not packaged)
-				godot # game engine
-				qtcreator # QT IDE
-				renderdoc # graphics programming debugger
-				rustlings # rust beginner exercises
-				scons # c++ meta build tool
-				unityhub # game engine
-				valgrind # c++ memory profiler
-			];
-		};
-	};
+    den.aspects.coding = {
+        homeManager =
+            { pkgs, ... }:
+            {
+                home.packages = with pkgs; [
+                    assimp # asset importing library
+                    bacon # rust code checker
+                    cargo-deny # cargo denpendency linting / license checking
+                    clang # clang compiler
+                    clang-tools # additional tools for clang
+                    gdb # c++ / rust compiler
+                    # glad # opengl function pointer loader (not packaged)
+                    godot # game engine
+                    # qtcreator # QT IDE
+                    renderdoc # graphics programming debugger
+                    rustlings # rust beginner exercises
+                    scons # c++ meta build tool
+                    unityhub # game engine
+                    valgrind # c++ memory profiler
+                ];
+            };
+    };
 }

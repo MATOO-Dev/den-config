763247b9f29608389ca10dc3efcf123d6e99f19c
MATOO-Dev <matoo-dev@proton.me>
2026-04-01 14:41:39 +0200

    Disabled bash integration for starship

--- modules/aspects/promt.nix
+++ modules/aspects/promt.nix
@@ -1,92 +1,93 @@
 {
-	den.aspects.prompt.homeManager = { lib, ... }: {
-		programs.starship = {
-			enable = true;
-			enableBashIntegration = true;
-			enableZshIntegration = true;
-			enableFishIntegration = true;
-			settings = {
-				add_newline = true;
-				scan_timeout = 10;
+    den.aspects.prompt.homeManager =
+        { lib, ... }:
+        {
+            programs.starship = {
+                enable = true;
+                enableBashIntegration = false;
+                enableZshIntegration = true;
+                enableFishIntegration = true;
+                settings = {
+                    add_newline = true;
+                    scan_timeout = 10;
 
-				palette = "rosepine";
+                    palette = "rosepine";
 
-				palettes.rosepine = {
-					theme01 = "#1f1d2e";
-					theme08 = "#eb6f92";
-					theme09 = "#f6c177";
-					theme0a = "#ebbcba";
-					theme0b = "#31748f";
-					theme0c = "#9ccfd8";
-					theme0d = "#c4a7e7";
-				};
+                    palettes.rosepine = {
+                        theme01 = "#1f1d2e";
+                        theme08 = "#eb6f92";
+                        theme09 = "#f6c177";
+                        theme0a = "#ebbcba";
+                        theme0b = "#31748f";
+                        theme0c = "#9ccfd8";
+                        theme0d = "#c4a7e7";
+                    };
 
+                    format = lib.strings.concatStrings [
+                        "$os"
+                        "[](bg:theme0a fg:theme09)"
+                        "$directory"
+                        "[](bg:theme0b fg:theme0a)"
+                        "$git_branch"
+                        "[](bg:theme0c fg:theme0b)"
+                        "$git_status"
+                        "[](fg:theme0c)"
+                        " "
+                    ];
 
-				format = lib.strings.concatStrings [
-					"$os"
-					"[](bg:theme0a fg:theme09)"
-					"$directory"
-					"[](bg:theme0b fg:theme0a)"
-					"$git_branch"
-					"[](bg:theme0c fg:theme0b)"
-					"$git_status"
-					"[](fg:theme0c)"
-					" "
-				];
+                    right_format = lib.strings.concatStrings [
+                        "$character"
+                        "\${env_var.ZVM_MODE}"
+                        "[](fg:theme0d)"
+                        "$time"
+                        "[](fg:theme0d)"
+                    ];
 
-				right_format = lib.strings.concatStrings [
-					"$character"
-					"\${env_var.ZVM_MODE}"
-					"[](fg:theme0d)"
-					"$time"
-					"[](fg:theme0d)"
-				];
+                    os = {
+                        disabled = false;
+                        format = "[ $symbol ](bg:theme09 fg:theme01)";
+                        symbols.NixOS = " ";
+                    };
 
-				os = {
-					disabled = false;
-					format = "[ $symbol ](bg:theme09 fg:theme01)";
-					symbols.NixOS = " ";
-				};
+                    directory = {
+                        disabled = false;
+                        format = "[ $path ](bg:theme0a fg:theme01)";
+                        truncation_length = 1;
+                        truncation_symbol = "../";
+                        substitutions = {
+                            "~" = " ";
+                            "Downloads" = " ";
+                            "Documents" = "󰈙 ";
+                            "Repos" = " ";
+                            "Dotfiles" = " ";
+                            "Rust" = " ";
+                        };
+                    };
 
-				directory = {
-					disabled = false;
-					format = "[ $path ](bg:theme0a fg:theme01)";
-					truncation_length = 1;
-					truncation_symbol = "../";
-					substitutions = {
-						"~" = " ";
-						"Downloads" = " ";
-						"Documents" = "󰈙 ";
-						"Repos" = " ";
-						"Dotfiles" = " ";
-						"Rust" = " ";
-					};
-				};
+                    git_branch = {
+                        disabled = false;
+                        symbol = "";
+                        format = "[ $symbol $branch ](bg:theme0b fg:theme01)";
+                    };
 
-				git_branch = {
-					disabled = false;
-					symbol = "";
-					format = "[ $symbol $branch ](bg:theme0b fg:theme01)";
-				};
+                    git_status = {
+                        disabled = false;
+                        ahead = " 󰁝$count ";
+                        behind = " 󰁅$count ";
+                        untracked = " ?$count ";
+                        modified = " !$count ";
+                        staged = " +$count ";
+                        renamed = " 󰄾$count ";
+                        deleted = " x$count ";
+                        format = "[$all_status$ahead$behind](bg:theme0c fg:theme01)";
+                    };
 
-				git_status = {
-					disabled = false;
-					ahead = " 󰁝$count ";
-					behind = " 󰁅$count ";
-					untracked = " ?$count ";
-					modified = " !$count ";
-					staged = " +$count ";
-					renamed = " 󰄾$count ";
-					deleted = " x$count ";
-					format = "[$all_status$ahead$behind](bg:theme0c fg:theme01)";
-				};
-
-				time = {
-					disabled = false;
-					time_format = "%R";
-					format = "[$time](bg:theme0d fg:theme01)";
-				};
-			};
-		};
-	};
+                    time = {
+                        disabled = false;
+                        time_format = "%R";
+                        format = "[$time](bg:theme0d fg:theme01)";
+                    };
+                };
+            };
+        };
 }

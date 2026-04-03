3163041de1fe118f0530899fbeb2336b833dd43d
MATOO-Dev <matoo-dev@proton.me>
2026-04-03 22:51:03 +0200

    Updated obsidian aspect

--- modules/aspects/obsidian.nix
+++ modules/aspects/obsidian.nix
@@ -1,59 +1,49 @@
 {
-	den.aspects.obsidian.homeManager = {
-		programs.obsidian = {
-			enable = true;
+    den.aspects.obsidian.homeManager = {
+        programs.obsidian = {
+            enable = true;
 
-			defaultSettings = {
-				corePlugins = {
-					"backlinks".enable = true;
-					"canvas".enable = true;
-					"quick-switcher".enable = true;
-					"sync" = {
-						enable = true;
-						settings = {
-
-						};
-					};
-					"templates" = {
-						enable = true;
-						settings = {
-
-						};
-					};
-				};
-				communityPlugins = {
-					"banners".enable = true;
-					"better-export-pdf".enable = true;
-					"better-word-count".enable = true;
-					"dataview".enable = true;
-					"diagrams".enable = true;
-					"excalidraw".enable = true;
-					"iconize".enable = true;
-					"kanban".enable = true;
-					"link-range".enable = true;
-					"mermaid-tools".enable = true;
-					"multi-tag".enable = true;
-					"note-refactor".enable = true;
-					"omnisearch".enable = true;
-					"paste-url-into-selection".enable = true;
-					"pdf++".enable = true;
-					"plantuml".enable = true;
-					"quick-latex".enable = true;
-					"recent-files".enable = true;
-					"style-settings".enable = true;
-					"surfing".enable = true;
-					"text-extractor".enable = true;
-					"typing-transformer".enable = true;
-					"typst-renderer".enable = true;
-					"vim-toggle".enable = true;
-					"wypst".enable = true;
-				};
-			};
-
-			vaults."computer_science" = {
-				enable = true;
-				target = "computer_science";
-			};
-		};
-	};
+            defaultSettings = {
+                app = {
+                    vimMode = true;
+                    newFileLocation = "current";
+                    showLineNumbers = true;
+                };
+                # corePlugins = [
+                #     "backlink"
+                #     "canvas"
+                #     "switcher"
+                #     "sync"
+                #     "templates"
+                # ];
+                # communityPlugins = [
+                # "banners"
+                # "better-export-pdf"
+                # "better-word-count"
+                # "dataview"
+                # "diagrams"
+                # "Excalidraw"
+                # "iconize"
+                # "kanban"
+                # "link-range"
+                # "mermaid-tools"
+                # "multi-tag"
+                # "note-refactor"
+                # "omnisearch"
+                # "paste-url-into-selection"
+                # "pdf++"
+                # "plantuml"
+                # "quick-latex"
+                # "recent-files"
+                # "style-settings"
+                # "surfing"
+                # "text-extractor"
+                # "typing-transformer"
+                # "typst-renderer"
+                # "vim-toggle"
+                # "wypst"
+                # ];
+            };
+        };
+    };
 }
--- modules/users/matoo.nix
+++ modules/users/matoo.nix
@@ -26,7 +26,7 @@
             den.aspects.neovim
             den.aspects.niri
             den.aspects.nix
-            # den.aspects.obsidian
+            den.aspects.obsidian
             den.aspects.prompt
             den.aspects.recording
             den.aspects.search

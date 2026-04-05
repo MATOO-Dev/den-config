50c0a48c66c91a2414f43d26130ccc108bc6e9e7
MATOO-Dev <matoo-dev@proton.me>
2026-04-05 20:22:42 +0200

    Added cargo and rustc

--- modules/aspects/coding.nix
+++ modules/aspects/coding.nix
@@ -4,16 +4,17 @@
             { pkgs, ... }:
             {
                 home.packages = with pkgs; [
+                    # glad # opengl function pointer loader (not packaged)
                     assimp # asset importing library
                     bacon # rust code checker
+                    cargo # rust package manager
                     cargo-deny # cargo denpendency linting / license checking
                     clang # clang compiler
                     clang-tools # additional tools for clang
                     gdb # c++ / rust compiler
-                    # glad # opengl function pointer loader (not packaged)
                     godot # game engine
-                    # qtcreator # QT IDE
                     renderdoc # graphics programming debugger
+                    rustc # rust compiler
                     rustlings # rust beginner exercises
                     scons # c++ meta build tool
                     unityhub # game engine

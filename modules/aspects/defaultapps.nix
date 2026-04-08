70712dcf5439f6f75716e40e831bc12df9794877
MATOO-Dev <matoo-dev@proton.me>
2026-04-08 14:48:57 +0200

    Fixed defaultapps aspect not being included

--- modules/aspects/defaultapps.nix
+++ modules/aspects/defaultapps.nix
@@ -1,13 +1,13 @@
 {
-    den.aspects.defaultapps.home-manager =
+    den.aspects.defaultapps.homeManager =
         { pkgs, ... }:
         {
             home.packages = [
                 # requires file input to work
                 # test if syntax is correct like this
                 (pkgs.writeShellScriptBin "find-mime-type" ''
-                    				${pkgs.file}/bin/file --mime-type -b
-                    			'')
+                    ${pkgs.file}/bin/file --mime-type -b
+                '')
             ];
 
             xdg.mimeApps = {
@@ -15,6 +15,7 @@
                 # types can be found using 'file --mime-type -b <file>`
                 defaultApplications = {
                     "applications/pdf" = "evince.desktop";
+                    "image/*" = "eog.dekstop";
                     "x-scheme-handler/http" = "firefox.desktop";
                     "x-scheme-handler/https" = "firefox.desktop";
                     "x-scheme-handler/about" = "firefox.desktop";

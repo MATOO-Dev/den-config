b12fd0e9fc8a525da480a2822a77b8b918fe6706
MATOO-Dev <matoo-dev@proton.me>
2026-04-12 11:44:32 +0200

    Enabled tray icon for syncthing

--- modules/aspects/syncthing.nix
+++ modules/aspects/syncthing.nix
@@ -2,6 +2,7 @@
 	den.aspects.syncthing.homeManager = { pkgs, ... }: {
 		services.syncthing = {
 			enable = true;
+			tray.enable = true;
 			# settings = { 
 			# 	devices = { };
 			#	folders = { };
